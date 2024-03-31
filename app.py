fbc0d273f8a79bb8d7874dcab876c4e1955d0468
loregi01 <gizzilorenzo@virgilio.it>
2024-03-31 11:15:20 +0200

    initialScheleton

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+connection = mysql.connector.connect(user='root', password='root', host='mysql', port="3306", database='db')
+print("DB connected")
+
+cursor = connection.cursor()
+cursor.execute('Select * FROM students')
+students = cursor.fetchall()
+connection.close()
+
+print(students) 
+
+@app.route('/')
+def hello():
+    return render_template("home_page.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
---
+++ mysql/database_students.sql
@@ -0,0 +1,10 @@
+use db;
+
+CREATE TABLE students (
+    StudentID int not null AUTO_INCREMENT,
+    FirstName varchar(100) NOT NULL,
+    Surname varchar(100) NOT NULL,
+    PRIMARY KEY (StudentID)
+);
+
+INSERT INTO students (FirstName, Surname) VALUES ("Lorenzo", "Gizzi");
